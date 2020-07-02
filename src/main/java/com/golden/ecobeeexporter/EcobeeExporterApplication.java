cbd839accdd8ea75ec81c1ffdf334a55040fdd19
Zack <ZGoldenblue@gmail.com>
2020-07-02 16:07:01 -0500

    Initial Commit

---
+++ src/main/java/com/golden/ecobeeexporter/EcobeeExporterApplication.java
@@ -0,0 +1,13 @@
+package com.golden.ecobeeexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcobeeExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcobeeExporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golden/ecobeeexporter/EcobeeExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golden.ecobeeexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcobeeExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
